2c60054419de528878319cdb2d68d5b239741fde
onghw84 <30618323+onghw84@users.noreply.github.com>
2025-02-28 19:52:17 +0800

    Create api.js

---
+++ sudoku_solver/api.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const SudokuSolver = require('../controllers/sudoku-solver.js');
+
+module.exports = function (app) {
+  
+  let solver = new SudokuSolver();
+
+  app.route('/api/check')
+    .post((req, res) => {
+
+      if (!(req.body.puzzle && req.body.coordinate && req.body.value)){
+        res.json({ error: 'Required field(s) missing' });
+      }
+
+      const puzzle = req.body.puzzle;
+      const coord = req.body.coordinate.toUpperCase();      
+      const value = req.body.value;
+      const valid = solver.validate(puzzle);
+      if (valid.error){
+        res.json(valid); return;
+      }
+      if (!value.match(/^([1-9])$/)){
+        res.json({ "error": "Invalid value" }); return;
+      }      
+      if (!coord.match(/^([A-I][1-9])$/)){
+        res.json({ "error": "Invalid coordinate" }); return;
+      }
+
+      const result = {}; result.valid = true; result.conflict = [];
+      if (solver.checkRowPlacement(puzzle, coord[0], coord[1], value)){
+        result.valid = false; result.conflict.push("row");
+      }
+
+      if (solver.checkColPlacement(puzzle, coord[0], coord[1], value)){
+        result.valid = false; result.conflict.push("column");
+      }
+
+      if (solver.checkRegionPlacement(puzzle, coord[0], coord[1], value)){
+        result.valid = false; result.conflict.push("region");      
+      }
+      if (result.conflict.length == 0){
+        res.json({"valid": "true"});        
+      }
+      else {res.json(result);}
+    });
+    
+  app.route('/api/solve')
+    .post((req, res) => {
+      if (!req.body.puzzle){
+        res.json({ error: 'Required field missing' }); return;
+      }
+      const puzzle = req.body.puzzle;
+      const valid = solver.validate(puzzle);
+      if (valid.error){
+        res.json(valid);
+      }
+      else {
+        const solution = solver.solve(puzzle);
+        res.json(solution);
+      }
+    });
+};
