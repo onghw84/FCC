28e45b056689f16a62e64460ee07c6f782e84e0d
onghw84 <30618323+onghw84@users.noreply.github.com>
2025-05-26 14:35:19 +0800

    Create Collectible.mjs

---
+++ real_time_mp_game/public/Collectible.mjs
@@ -0,0 +1,19 @@
+class Collectible {
+  constructor({x, y, value, id, radius = 20}) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+    this.id = id;
+    this.radius = radius;
+  }
+}
+
+/*
+  Note: Attempt to export this for use
+  in server.js
+*/
+try {
+  module.exports = Collectible;
+} catch(e) {}
+
+export default Collectible;
